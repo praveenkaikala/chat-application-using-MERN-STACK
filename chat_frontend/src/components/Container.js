f180eeefd5e71aca674401ac0332dd857df93bd9
praveenkaikala <praveenkaikala25@gmail.com>
2024-01-26 10:54:37 +0530

    backend committ

--- chat_frontend/src/components/Container.js
+++ chat_frontend/src/components/Container.js
@@ -1,29 +1,36 @@
-import React, { createContext, useState } from 'react'
-import "./styles.css"
-import Slidebar from './Slidebar'
-import Workarea from './Workarea'
-import Welcome from './Welcome'
-import Creategroups from './Creategroups'
-import Onlineusers from './Onlineusers'
-import { Outlet } from 'react-router-dom'
-import { Circle } from '@mui/icons-material'
-export const mycontext=createContext()
+import React, { createContext, useState } from "react";
+import "./styles.css";
+import Slidebar from "./Slidebar";
+import Workarea from "./Workarea";
+import Welcome from "./Welcome";
+import Creategroups from "./Creategroups";
+import Onlineusers from "./Onlineusers";
+import { Outlet } from "react-router-dom";
+import { Circle } from "@mui/icons-material";
+export const mycontext = createContext();
 const Container = () => {
-  const [chatload,setchatload]=useState(false)
-  const [refresh,setrefresh]=useState(true)
+  const [chatload, setchatload] = useState(false);
+  const [refresh, setrefresh] = useState(true);
   return (
-    <div className='container'>
-      <mycontext.Provider value={{refresh:refresh,setrefresh:setrefresh,chatload:chatload,setchatload:setchatload}}>
-        <Slidebar/>
-        <Outlet/>
+    <div className="container">
+      <mycontext.Provider
+        value={{
+          refresh: refresh,
+          setrefresh: setrefresh,
+          chatload: chatload,
+          setchatload: setchatload,
+        }}
+      >
+        <Slidebar />
+        <Outlet />
       </mycontext.Provider>
-        
-        {/* <Onlineusers/> */}
-        {/* <Creategroups/> */}
-        {/* <Welcome/> */}
-        {/* <Workarea /> */}
-    </div>
-  )
-}
 
-export default Container
+      {/* <Onlineusers/> */}
+      {/* <Creategroups/> */}
+      {/* <Welcome/> */}
+      {/* <Workarea /> */}
+    </div>
+  );
+};
+
+export default Container;
